be7b0260c7748219a2a91fc46bf05272aee1d215
Doanh Trần <hoainguyenhc03@gmail.com>
2022-10-26 23:04:11 +0700

    buoi2

---
+++ src/bai1/BAI1.java
@@ -0,0 +1,26 @@
+package bai1;
+
+public class BAI1 {
+	public static void main(String[] args) {
+		//happy
+	     System.out.println("*     *    *    ****  ****  *   *");  
+	     System.out.println("|     |   / \\   |   * |   *  \\ / ");
+	     System.out.println("*-----*  *---*  ****  ****    *");
+	     System.out.println("|     | /     \\ |     |       |");
+	     System.out.println("*     **       **     *       *\n\n");
+	     
+	     //New
+	     System.out.println("*   *  ****  *       *       *");
+	     System.out.println("|\\  |  |      \\     / \\     /");
+	     System.out.println("* * *  ****    *   *   *   *");
+	     System.out.println("|  \\|  |        \\ /     \\ /");
+	     System.out.println("*   *  ****      *       *\n\n");
+	     
+	     //year
+	     System.out.println("*   * ****     *     ****");
+	     System.out.println(" \\ /  |       / \\    |   *");
+	     System.out.println("  *   ****   *---*   ****");
+	     System.out.println("  |   |     /     \\  |   \\");
+	     System.out.println("  *   **** *       * *    *");
+	}
+}
